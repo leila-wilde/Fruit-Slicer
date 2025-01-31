d3933d7912f10f8e1340f4b36f6b2b059bb00b6a
morgane-du-tweize <morgane_rt@hotmail.com>
2025-01-31 09:27:52 +0100

    coded a function that manages ice cube

---
+++ generating_new_item.py
@@ -0,0 +1,30 @@
+# to manage the ice cube :
+# we use a boolean : "ice_cube"
+
+# time.time() tells us when new item generation
+# is cooled down by a ice cube
+
+# after 3 seconds :
+# boolean ice_cube = False
+
+# if ice_cube == True :
+    # generating a new fruit is disable
+def generate_new_fruit(ice_cube, time_stamp):
+
+    # if time is frozen
+    # cannot work for 3 seconds
+
+    # is called in while True loop
+    # every seconds
+
+    # generates randomly a item name
+    # among a array of names
+    # generates randomly a letter
+    # generates randomly a horizontal position
+    # vertical position = top of the screen
+    # return a tuple
+    # (item_name, letter, (x, y))
+
+    # finally new fruit is added to the list
+    # alive_items
+    pass
