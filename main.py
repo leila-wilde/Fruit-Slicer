cf349c425ecb508659099712d6042a1bdea1e9ea
anna-leite <anna.leite@laplateforme.io>
2025-01-31 13:10:37 +0100

    game_over_screen okay

--- main.py
+++ main.py
@@ -40,21 +40,37 @@ def game_over_screen(total_score):
     background_image_game = pygame.transform.scale(background_image_game, (WIDTH, HEIGHT)) 
     DISPLAYSURF.blit(background_image_game, (0, 0))
 
-    blood_image = pygame.image.load("assets/flaque1.png").convert_alpha()
-    blood_image = pygame.transform.smoothscale(blood_image, (500, 500))
-    DISPLAYSURF.blit(blood_image, (150, 50))
+    # blood_image = pygame.image.load("assets/flaque1.png").convert_alpha()
+    # blood_image = pygame.transform.smoothscale(blood_image, (500, 500))
+    # DISPLAYSURF.blit(blood_image, (150, 50))
 
     game_over_text = "GAME OVER"
     font_game_over_text = pygame.font.Font('fonts/DoubleFeature20.ttf', 100)
     game_over_text_surf = font_game_over_text.render(game_over_text, True, RED)
-    game_over_text_rect = game_over_text_surf.get_rect(center=(MID_WIDTH, MID_HEIGHT))
+    game_over_text_rect = game_over_text_surf.get_rect(center=(MID_WIDTH, MID_HEIGHT - 100))
     DISPLAYSURF.blit(game_over_text_surf, game_over_text_rect)
 
     score_text = f"ton score est : {total_score} !"
     font_score_text = pygame.font.Font('fonts/DoubleFeature20.ttf', 40)
     score_text_surf = font_score_text.render(score_text, True, RED)
-    score_text_rect = score_text_surf.get_rect(center = (MID_WIDTH, 450))
+    score_text_rect = score_text_surf.get_rect(center = (MID_WIDTH, MID_HEIGHT + 20))
     DISPLAYSURF.blit(score_text_surf, score_text_rect)
+
+    while True: 
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        button_rect = draw_button("MENU", MID_WIDTH, 550, button_rect.collidepoint(mouse_x, mouse_y) if 'button1_rect' in locals()else False)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN and button_rect.collidepoint(mouse_x, mouse_y):
+                menu()
+
+        pygame.display.update()
+
+
 
 def display_score(total_score):
     score = f"score : {total_score}"
@@ -80,7 +96,6 @@ def menu():
     DISPLAYSURF.blit(background_image, (0, 0)) 
 
     # Display title
-    # Display title
     text = "FRUIT SLICER"
     font_text = pygame.font.Font('fonts/DoubleFeature20.ttf', 100)
     text_surf = font_text.render(text, True, RED)
@@ -98,7 +113,8 @@ def menu():
                 sys.exit()
 
             if event.type == pygame.MOUSEBUTTONDOWN and button1_rect.collidepoint(mouse_x, mouse_y):
-                game_loop()
+                total_score = game_loop()
+                game_over_screen(total_score)
 
             if event.type == pygame.MOUSEBUTTONDOWN and button2_rect.collidepoint(mouse_x, mouse_y):
                 # add code
@@ -120,7 +136,7 @@ def create_random_item():
     image_surface = pygame.image.load("assets/" + item + ".png").convert_alpha()
     image_surface = pygame.transform.scale(image_surface, (85, 85))
     font = pygame.font.Font(None, 40) # attention changer la font !!!
-    letter_render = font.render(letter, True, (255, 255, 255))
+    letter_render = font.render(letter, True, (RED))
     image_surface.blit(letter_render, (10, 60))
 
     return item, letter, y, image_surface, image_surface.get_rect(topleft=(x, y))
@@ -131,7 +147,6 @@ def game_loop():
     generate_new_item = 0
     lives = 3
     total_score =0
-    fail = False
     current_score = 0
 
     run = True
@@ -149,9 +164,6 @@ def game_loop():
                         print(f"letter input : {letter_input}")
                         print(f"fruit : {target_list[i]}")
                         if target_list[i]['item'] == "grenade":
-
-                            # fail is for scoring
-                            fail = True
                             run = False
                         elif target_list[i]['item'] == "glacon":
                             print("gele temps == freezes time")
@@ -159,22 +171,19 @@ def game_loop():
                             # for 3 seconds
                             current_score += 1
                         else :
-                            current_score += 1   
+                            current_score += 1
             
+                # combo score
+                if current_score > 2:
+                    current_score -= 1  
 
+                total_score += current_score
+                current_score = 0
+            
                 # when user presses a key that is equal to the letter of the item
                 # or the letter of more than 1 item
                 # corresponding items disappear
                 target_list = [obj for obj in target_list if obj['letter'] != letter_input]
-
-            if fail:
-                current_score = 0
-
-            if current_score > 2:
-                current_score -= 1
-
-            total_score += current_score
-
 
         # Variable that controls the frequency of new items being created. (if FPS = 60 then 30 frames = 0.5 seconds)
         generate_new_item += 2 # increments by 2 on each iteration of the loop
@@ -188,7 +197,7 @@ def game_loop():
             obj['rect'].y += DROP_SPEED  # we can change this speed in the constants 
 
         # Clear the screen
-        background_image = pygame.image.load("assets/massacre_tronconneuse.jpg")
+        background_image = pygame.image.load("assets/massacre.jpg")
         background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))
         DISPLAYSURF.blit(background_image, (0, 0))
 
@@ -217,7 +226,7 @@ def game_loop():
         pygame.display.flip()
         clock.tick(FPS)
 
-    game_over_screen(total_score)
+    return total_score
 
 
 # Main function to run the game
